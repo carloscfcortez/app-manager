e8214c0e75f2643699a8218557c36d4c5c5d5418
Carlos Cortez <carlos.cfcortez@gmail.com>
2020-11-13 19:16:50 -0400

    integration tests

---
+++ api/src/AppManager.IntegrationTests/AppFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace AppManager.IntegrationTests
+{
+    public class AppFactory<T> : WebApplicationFactory<T> where T : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.UseStartup<T>();
+            builder.UseEnvironment("Development");
+        }
+    }
+}
---
+++ api/src/AppManager.IntegrationTests/FixtureCollection.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace AppManager.IntegrationTests
+{
+    [CollectionDefinition(nameof(FixtureCollection))]
+    public class FixtureCollection: ICollectionFixture<Fixture<StartupApiTests>>
+    {
+    }
+
+    public class Fixture<T> : IDisposable where T : class
+    {
+        public readonly AppFactory<T> factory;
+        public HttpClient client;
+
+        public Fixture()
+        {
+            var options = new WebApplicationFactoryClientOptions()
+            {
+                HandleCookies = false,
+                BaseAddress = new Uri("http://localhost:5000"),
+                AllowAutoRedirect = true,
+                //MaxAutomaticRedirections = 7
+            };
+
+            factory = new AppFactory<T>();
+            client = factory.CreateClient(options);
+
+        }
+
+        public void Dispose()
+        {
+            client.Dispose();
+            factory.Dispose();
+        }
+
+    }
+}
