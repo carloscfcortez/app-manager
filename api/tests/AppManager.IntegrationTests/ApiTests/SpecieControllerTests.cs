2e3fed06699635349bdca17e730495762d0a2e08
Carlos Cortez <carlos.cfcortez@gmail.com>
2020-11-16 16:17:19 -0400

    integration tests

---
+++ api/tests/AppManager.IntegrationTests/ApiTests/SpecieControllerTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using AppManager.Application.DTO;
+using AppManager.Application.Interfaces;
+using AppManager.Domain.Entities;
+using AppManager.Services.Api;
+using AppManager.Services.Api.Controllers;
+using AutoMapper;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Moq;
+using Xunit;
+
+namespace AppManager.IntegrationTests.ApiTests
+{
+    public class SpecieControllerTests : IntegrationTest
+    {
+        [Fact]
+        public async Task GetAll_WithoutParams_ReturnAll()
+        {
+            // Act
+            var response = await Client.GetAsync("api/specie");
+
+            var result = response.Content.ReadAsStringAsync();
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            response.Content.ReadAsAsync<List<TreeDTO>>().Should().NotBeNull();
+
+        }
+    }
+}
---
+++ api/tests/AppManager.IntegrationTests/IntegrationTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using AppManager.Infrastructure.Data.Context;
+using AppManager.Services.Api;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace AppManager.IntegrationTests
+{
+    public class IntegrationTest
+    {
+        protected readonly HttpClient Client;
+        public IntegrationTest()
+        {
+
+            var appFactory = new WebApplicationFactory<Startup>()
+            .WithWebHostBuilder(builder =>
+            {
+                //builder.ConfigureAppConfiguration(x =>
+                //{
+
+                //});
+
+                builder.ConfigureServices(services =>
+                {
+                    var descriptor = services
+                    .SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<DataContext>));
+                    services.Remove(descriptor);
+
+                    //services.RemoveAll<DataContext>();
+                    services.AddDbContext<DataContext>(opt =>
+                    {
+                        opt.UseInMemoryDatabase("AppManagerTest");
+                    });
+
+
+                    var sp = services.BuildServiceProvider();
+
+                    using (var scope = sp.CreateScope())
+                    {
+                        var scopedServices = scope.ServiceProvider;
+                        var db = scopedServices.GetRequiredService<DataContext>();
+
+                        db.Database.EnsureCreated();
+
+                        try
+                        {
+                            InitializeDbTests(db);
+                        }
+                        catch (Exception ex)
+                        {
+                            throw ex;
+                        }
+                    }
+
+                });
+            });
+
+            Client = appFactory.CreateClient();
+
+        }
+
+        public static void InitializeDbTests(DataContext context)
+        {
+            
+        }
+
+    }
+}
